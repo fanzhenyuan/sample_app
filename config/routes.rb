a219077121a35bc02d5068f535c89bea4b3cf413
fanzhenyuan <fanyfan2020@hotmail.com>
2020-06-17 20:15:39 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  
   get '/signup', to: 'users#new'
   
   get '/home', to: 'static_pages#home'
@@ -6,5 +8,6 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   
-  root 'static_pages#home'
+  resources :users
+
 end
